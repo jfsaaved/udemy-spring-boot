2b68ca0d0f9c1c1338106d472e3ab744d46a7d90
Julian Saavedra <julian.felipe.saavedra@gmail.com>
2017-01-26 17:08:17 -0500

    January 26, 2017 - Commit 2 - YAML

--- src/main/java/com/example/HomeController.java
+++ src/main/java/com/example/HomeController.java
@@ -1,15 +1,21 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
 @RestController
 public class HomeController {
 	
+	@Value("${pageController.msg}")
+	private String pageControllerMsg;
+	
 	@RequestMapping("/")
 	public String home(){
-		return "Hello World!!";
+		return pageControllerMsg;
 	}
 
 }
