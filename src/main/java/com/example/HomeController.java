903b79d405e64e26b760296a8c20a5fa4549b72d
Julian Saavedra <julian.felipe.saavedra@gmail.com>
2017-01-25 20:31:59 -0500

    January 25, 2017 - Commit 1 - DevTools

--- src/main/java/com/example/HomeController.java
+++ src/main/java/com/example/HomeController.java
@@ -1,12 +1,15 @@
-package com.example.home;
+package com.example;
 
+import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+	
 	@RequestMapping("/")
-	public String sayHelloWorld() {
-		return "Hello World";
+	public String home(){
+		return "Hello World!!";
 	}
+
 }
